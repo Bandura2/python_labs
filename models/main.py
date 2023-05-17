f318bffbb909e8b780701c36acdc0870c51b4a22
Bandura2 <119165162+Bandura2@users.noreply.github.com>
2023-05-18 01:34:57 +0300

    Edited lab after delivery

--- models/main.py
+++ models/main.py
@@ -1,6 +1,14 @@
-class Insect:
-    instance = None
+"""python package with models"""
 
+
+class Insect:
+    """
+    Create class Insect with fields: name, number_of_legs, has_wings,
+     is_dangerous, is_sleeping and methods: is_poisonous, hibernate, wake_up
+    """
+    __instance = None
+
+    # pylint: disable= too-many-arguments
     def __init__(self, name="", number_of_legs=0,
                  has_wings=False, is_dangerous=False, is_sleeping=False):
         self.name = name
@@ -10,31 +18,56 @@ class Insect:
         self.is_sleeping = is_sleeping
 
     def is_poisonous(self) -> bool:
+        """Method return field is_dangerous"""
         return self.is_dangerous
 
     def hibernate(self) -> None:
+        """Method assigns a variable is_sleeping True"""
         self.is_sleeping = True
 
     def wake_up(self) -> None:
+        """Method assigns a variable is_sleeping False"""
         self.is_sleeping = False
 
     @staticmethod
     def get_instance():
-        if not Insect.instance:
-            Insect.instance = Insect()
-        return Insect.instance
+        """Method return singleton field instance"""
+        if not Insect.__instance:
+            Insect.__instance = Insect()
+        return Insect.__instance
 
     def __str__(self):
         return f"Insect(name={self.name}, number_of_legs={self.number_of_legs}," \
                f" has_wings={self.has_wings}, is_dangerous={self.is_dangerous}," \
                f" is_sleeping={self.is_sleeping})"
 
+    @staticmethod
+    def count_of_element(*args) -> int:
+        """Method counts int in list"""
+        count = 0
+
+        for element in args:
+            count += 1
+
+        print(f"count = {count}")
+        return count
+
 
 if __name__ == '__main__':
-    array_insects = [Insect("Mantis", 6, False, True, False),
-                     Insect(),
-                     Insect.get_instance(),
-                     Insect.get_instance()]
+    list_insects = [
+        Insect("Mantis", 6, False, True, False),
+        Insect(),
+        Insect.get_instance(),
+        Insect.get_instance()
+    ]
 
-    for insect in array_insects:
+    for insect in list_insects:
         print(insect)
+
+    test_list = []
+
+    for i in range(10):
+        test_list.append(i)
+
+    print(Insect.count_of_element(1, 2, 3))
+    print(Insect.count_of_element(*test_list))
