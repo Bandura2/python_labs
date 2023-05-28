f4c8bf06848dcb761eb5d34baa59c1fa6aaa09e2
Bandura2 <119165162+Bandura2@users.noreply.github.com>
2023-05-28 14:41:52 +0300

    Add models for third python lab

--- models/main.py
+++ models/main.py
@@ -10,13 +10,14 @@ class AbstractClassInsect(ABC):
     __instance = None
 
     # pylint: disable= too-many-arguments
-    def __init__(self, name="", number_of_legs=0,
-                 has_wings=False, is_dangerous=False, is_sleeping=False):
+    def __init__(self, name="", number_of_legs=0, has_wings=False,
+                 is_dangerous=False, is_sleeping=False, favorite_food_set=None):
         self.name = name
         self.number_of_legs = number_of_legs
         self.has_wings = has_wings
         self.is_dangerous = is_dangerous
         self.is_sleeping = is_sleeping
+        self.favorite_food_set = favorite_food_set or set()
 
     def is_poisonous(self):
         """
@@ -25,11 +26,15 @@ class AbstractClassInsect(ABC):
         return self.is_dangerous
 
     def hibernate(self):
-        """Method assigns a variable is_sleeping True"""
+        """
+        Method assigns a variable is_sleeping True
+        """
         self.is_sleeping = True
 
     def wake_up(self):
-        """Method assigns a variable is_sleeping False"""
+        """
+        Method assigns a variable is_sleeping False
+        """
         self.is_sleeping = False
 
     @staticmethod
@@ -50,6 +55,8 @@ class AbstractClassInsect(ABC):
     def count_of_element(*args):
         """
         Method counts how many elements are passed to it
+        :param args: parameter allows you to pass any number of positional arguments to function
+        :return: count of element that passed to it
         """
         count = 0
 
@@ -68,3 +75,16 @@ class AbstractClassInsect(ABC):
     @abstractmethod
     def survive_over_winter(self):
         pass
+
+    def get_fields_by_type(self, type_field):
+        """
+        Method create dictionary with all attributes and values of the object,
+        filtered by the specified value type
+        :param type_field: type fields to filter
+        :return: a dictionary with all attributes and values of the object,
+        filtered by the specified value type
+        """
+        return {key: value for key, value in self.__dict__.items() if isinstance(value, type_field)}
+
+    def __iter__(self):
+        return self.favorite_food_set
